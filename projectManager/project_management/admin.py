a820abfed35e6cc8ed4ce18043616bad12bd75ca
samuelyohannis <samiyoni44@gmail.com>
2021-05-05 11:41:31 +0300

    new

--- projectManager/project_management/admin.py
+++ projectManager/project_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(RegionProject)
+admin.site.register(CityProject)
+admin.site.register(KebeleProject)
+admin.site.register(WeredaProject)
+admin.site.register(ZoneProject)
+admin.site.register(CountryProject)
