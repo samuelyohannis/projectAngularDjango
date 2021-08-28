a335add4895fb77a744a9a5c20b966030de0f623
samuelyohannis <samiyoni44@gmail.com>
2021-08-28 10:33:06 +0300

    new

--- projectManager/notification/consumers.py
+++ projectManager/notification/consumers.py
@@ -33,7 +33,7 @@ class NotificationConsumer(AsyncWebsocketConsumer):
        await self.accept() 
       
        await self.channel_layer.group_send(self.room_group_name,
-                                           {"type":"test1","tester":"You are connected to notification stream"}) 
+                                           {"type":"notications","notications":"You are connected to notification stream"}) 
        self.uuid3=[]
        def my(sender,instance=None,created=None,dispatch_uid=None,**kwargs,) :
             
@@ -46,20 +46,32 @@ class NotificationConsumer(AsyncWebsocketConsumer):
             await sync_to_async(post_save.connect)(my,sender=Notification,dispatch_uid=uuid.uuid4(),weak=False)      
        if self.room_group_name == "notification_country_project_manager":
             await sync_to_async(post_save.connect)(my,sender=Notification,dispatch_uid=uuid.uuid4(),weak=False)
+  
+   @database_sync_to_async
    def get_notifications(self):
         return Notification.objects.all()
-
+   
    @database_sync_to_async
    def get_serialized_notifications(self):
     return NotificationSerializer(Notification.objects.all(),many=True).data
+
    @database_sync_to_async
    def get_serialized_user(self,id):
     return UserSerializer(User.objects.get(pk=id)).data
+
    @database_sync_to_async
    def get_serialized_profile(self,id):
     return ProfileSerializer(Profile.objects.get(pk=id)).data
-   async def test1(self,event):
-          tester = event['tester'] 
+
+   @database_sync_to_async
+   def get_serialized_current_user(self):
+    return UserSerializer(User.objects.get(pk=self.scope["user"].id)).data
+
+   @database_sync_to_async
+   def get_serialized_current_profile(self):
+    return ProfileSerializer(Profile.objects.get(user=self.scope["user"].id)).data
+
+   async def notications(self,event):
           notifications = await self.get_serialized_notifications()
           await self.send(text_data=json.dumps({"notifications":notifications,"message":"You are connected to notification stream"}))
    async def test(self,event):
